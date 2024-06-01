1df1c824e5ef6a32b642f6a92382580c239c9753
2533757653 <72253387+2533757653@users.noreply.github.com>
2024-06-01 16:02:46 +0800

    Update file

---
+++ allclass.py
@@ -0,0 +1,41 @@
+
+class resource:
+    def __init__(self) -> None:
+        pass
+    def initize(self,id,name,number,startdate,enddate,starttime,endtime,priority,time):
+        self.id=id
+        self.name=name
+        self.number=number
+        self.startdate=startdate
+        self.enddate=enddate
+        self.starttime=starttime
+        self.endtime=endtime
+        self.priority=priority
+        self.time=time
+    def initfromlist(self,data):
+        self.id=data[0]
+        self.name=data[1]
+        self.number=data[2]
+        self.startdate=data[3]
+        self.enddate=data[4]
+        self.starttime=data[5]
+        self.endtime=data[6]
+        self.priority=data[7]
+        self.time=data[8]
+# 这里对应所有的资源,不过需要考虑一个问题，那就是异步处理属于是
+# 每个资源在使用结束后都需要释放，如何做呢？
+# 现实世界总要解决异步问题 
+
+class gongxu:
+    def __init__(self,name,pretime,worktime,aftertime) -> None:
+        self.name=name
+        #only pretime and worktime use resources
+        self.pretime=pretime
+        self.worktime=worktime
+        self.aftertime=aftertime
+        pass
+class gongyiluxian:
+    def __init__(self,id,gongxu) -> None:
+        self.id=id
+        self.gongxu=gongxu
+        
--- read.py
+++ read.py
@@ -1,13 +1,40 @@
 from openpyxl import load_workbook
-
+from allclass import *
+def readresource(path):
+    workbook = load_workbook(path)
+    sheet=workbook['资源日历']
+    id=0
+    allresource=[]
+    for row in sheet.iter_rows(values_only=True):
+            # 检查行是否为空
+        if(id==0):
+            id+=1
+            continue
+        tem=resource()
+        data=[]
+        for id,col in enumerate(row):
+            if(col!=None):
+                data.append(col)
+            elif(col==None and id==1):
+                data.append('箔绕人员')
+            elif(id>=8):
+                continue
+            else:
+                break
+        else:
+            print(data)
+            tem.initfromlist(data)
+            allresource.append(tem)
+    return allresource
+    
 def readquizfile(path):
     # 加载工作簿
     shengchangongdan=[{'工单编号':'', '物料编号':'', '数量':'', '计划开始日期':'', '计划完工日期':'', '前置工单':'', '优先级':''}]
     gongyiluxian=[{'物料编码':'', '工序':'', '前置工序':'', '资源名称':'', '资源需求':'', '准备工时min\n（固定值，资源占用）':'', '作业工时min\n（单位值，资源占用）':'', '后置工时min（固定值，不占用资源）':''}]
     ziyuanrili=[{'资源编号':'', '资源名称':'', '资源数量':'', '开始日期':'', '结束日期':'', '开始时间\n（包含）':'', '结束时间\n(包含)':'', '优先级':'', '班次':''}]
     gongdangongxu=[{'工单编号':'', '物料编号':'', '工序':'', '资源池':'', '资源ID':'', '资源需求':'', '开始时间':'', '结束时间':''}]
-    gongxurenwu=[{}] #?
-    paichengjieguo=[{}]
+    gongxurenwu=[{}]   #?
+    paichengjieguo=[{}]  #?
     workbook = load_workbook(path)
 
     # 获取所有工作表名
@@ -30,6 +57,7 @@ def readquizfile(path):
                 continue  # 如果行为空，则跳过
             # 打印非空行数据
             print(row)
+    return shengchangongdan,gongyiluxian,ziyuanrili,gongdangongxu,gongxurenwu,paichengjieguo
 
 # 用法示例
-readquizfile('data.xlsx')
+readresource('data.xlsx')
